9418dd824bc625e4846a2360de05242083bac197
Muamer Kukic <kukicako@gmail.com>
2019-10-11 11:01:56 -0700

    Styling done

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./App";
+import {createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from "redux-logger";
+import thunk from "redux-thunk"
 
-
-const store = createStore(rootReducer, applyMiddleware(thunk, logger))
-
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
 <Provider store = {store}>
